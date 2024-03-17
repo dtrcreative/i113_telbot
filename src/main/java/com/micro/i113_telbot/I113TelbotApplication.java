fe85ce57ba36f2f04cd2b99843d1572ec895a9bd
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-03-17 11:03:07 +0300

    Initial commit

---
+++ src/main/java/com/micro/i113_telbot/I113TelbotApplication.java
@@ -0,0 +1,13 @@
+package com.micro.i113_telbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I113TelbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I113TelbotApplication.class, args);
+    }
+
+}
