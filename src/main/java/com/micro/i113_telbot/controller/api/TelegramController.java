5d108a67e651465f6297f66936b5697d74d71372
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-03-24 16:51:18 +0300

    Added: handle multiple dto units

--- src/main/java/com/micro/i113_telbot/controller/api/TelegramController.java
+++ src/main/java/com/micro/i113_telbot/controller/api/TelegramController.java
@@ -1,11 +1,14 @@
 package com.micro.i113_telbot.controller.api;
 
+import com.micro.i113_telbot.model.dto.TelegramMessageDto;
 import com.micro.i113_telbot.model.dto.TelegramRegisterDto;
 import com.micro.i113_telbot.service.TelegramAPIService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/telbot/")
@@ -25,6 +28,11 @@ public class TelegramController {
     @PostMapping("/reg")
     public TelegramRegisterDto registerUser(@RequestBody TelegramRegisterDto unitDto) {
         return service.register(unitDto);
+    }
+
+    @PostMapping("/")
+    public void multipleDto(@RequestBody List<TelegramMessageDto> unitsDtoList) {
+        service.handleMultipleDto(unitsDtoList);
     }
 
 }
--- src/main/java/com/micro/i113_telbot/service/TelegramAPIService.java
+++ src/main/java/com/micro/i113_telbot/service/TelegramAPIService.java
@@ -1,12 +1,18 @@
 package com.micro.i113_telbot.service;
 
+import com.micro.i113_telbot.controller.telegrm.TelegramBot;
+import com.micro.i113_telbot.model.UserStatus;
+import com.micro.i113_telbot.model.dto.TelegramMessageDto;
 import com.micro.i113_telbot.model.dto.TelegramRegisterDto;
 import com.micro.i113_telbot.model.entity.UserEntity;
+import com.micro.i113_telbot.model.telegram.TelegramResponse;
 import com.micro.i113_telbot.repository.UserRepository;
 import com.micro.i113_telbot.service.converter.RegistrationConverter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,6 +21,7 @@ public class TelegramAPIService {
 
     private UserRepository repository;
     private RegistrationConverter converter;
+    private TelegramBot bot;
 
     public TelegramRegisterDto register(TelegramRegisterDto registerDto){
         registerDto.setUserSecretKey(generateRandomSecretKey());
@@ -28,6 +35,24 @@ public class TelegramAPIService {
             return converter.convertToDto(entity.get());
         }
         return registerDto;
+    }
+
+    public void handleMultipleDto(List<TelegramMessageDto> dtos){
+        for(TelegramMessageDto dto: dtos){
+            Optional<UserEntity> user = repository.findUserEntityByUserId(dto.getUserId());
+            if(user.isPresent() && user.get().getStatus().equals(UserStatus.ACTIVE)){
+                TelegramResponse response = TelegramResponse.builder()
+                        .user(user.get())
+                        .message(dto.getMessage())
+                        .build();
+                try{
+                    bot.sendMessage(response);
+                }catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
     }
 
     private String generateRandomSecretKey(){
